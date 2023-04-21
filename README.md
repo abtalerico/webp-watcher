e59b997f0721565ed31f40975f554201f928f1a7
Aidan Talerico <abtalerico@gmail.com>
2023-04-20 20:46:15 -0500

    feat: implement watching with inotify and conversion with pillow

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WebP Watcher
 A utility to watch a directory (usually `~/Downloads`) for webp files and automatically convert them to a more widely accepted format.
+
+```shell
+Usage: webp-watcher.py watch [OPTIONS] DIRECTORY
+
+Options:
+  --output-format TEXT  The output format for converted images. Valid values
+                        are 'png' and 'jpg'.
+  --help                Show this message and exit.
+```
--- webp-watcher.py
+++ webp-watcher.py
@@ -1,7 +1,39 @@
 #!/usr/bin/env python
 from PIL import Image
 import inotify.adapters
+import inotify.constants
 import click
+import os
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument("directory")
+@click.option("--output-format", help="The output format for converted images. Valid values are 'png' and 'jpg'.",
+              default="jpg")
+def watch(directory, output_format="jpg"):
+    if output_format != "jpg" and output_format != "png":
+        raise ValueError("Output format must be 'jpg' or 'png'.")
+    i = inotify.adapters.Inotify()
+    i.add_watch(directory, inotify.constants.IN_MOVED_TO | inotify.constants.IN_CLOSE_WRITE)
+
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, path, filename) = event
+        if os.environ.get("DEBUG"):
+            print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(path, filename, type_names))
+        if filename[-4:] == "webp":
+            im = Image.open(path + "/" + filename)
+            conv = im.convert("RGB")
+            output_filename = directory + "/" + filename.replace("webp", output_format)
+            conv.save(output_filename)
+            im.close()
+            conv.close()
+
 
 if __name__ == '__main__':
-    print("Hello!")
+    cli.add_command(watch)
+    cli()
